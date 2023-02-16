93df3358e4fcb928c4c95249f0bb913cb27e0736
Mervin Fansler <mef3005@med.cornell.edu>
2023-02-16 18:38:26 -0500

    inject flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+
+## See https://github.com/conda-forge/r-base-feedstock/issues/163#issuecomment-989515053
+sed -ie 's/PKG_CPPFLAGS =/PKG_CPPFLAGS= -DHAVE_WORKING_LOG1P/g'
+
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
